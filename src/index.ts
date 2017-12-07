882d2618660a92a8d01b4835fe4d95c46530d0d1
headdetect <headdetective2@gmail.com>
2017-12-07 01:47:40 -0700

    Refactor : Moving gatekeep module to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,38 @@
-export * from './module/gatekeep.module';
+import {ModuleWithProviders, NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {SuccessAnimationComponent} from "./module/component/success-animation.component";
+import {CapitalizePipe} from "./module/pipes/capitalize.pipe";
+import {BaseService} from "./module/service/base-service";
+import {EventsService} from "./module/service/events.service";
+import {LoginService} from "./module/service/login.service";
+
 export * from './module/models/';
 export * from './module/service/';
 export * from './module/component/';
 export * from './module/pipes';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    SuccessAnimationComponent,
+    CapitalizePipe
+  ],
+  declarations: [
+    SuccessAnimationComponent,
+    CapitalizePipe
+  ],
+})
+export class GatekeepModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: GatekeepModule,
+      providers: [
+        BaseService,
+        EventsService,
+        LoginService
+      ]
+    };
+  }
+}
