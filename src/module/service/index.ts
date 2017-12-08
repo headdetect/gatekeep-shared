e8aa5620263ec640f0e67ddb4490a596fe09405c
Alex Anderson <alex.anderson2209@gmail.com>
2017-12-07 23:53:46 -0700

    Added overview to index.ts

--- src/module/service/index.ts
+++ src/module/service/index.ts
@@ -1,5 +1,6 @@
 export * from './events.service';
 export * from './login.service';
+export * from './overview.service';
 export * from './visitor.service';
 export * from './volunteer.service';
 export * from './base-service';
