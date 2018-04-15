9ed4d41143436d453f631b7eb309b60277dd056b
amouritsen <amouritsen@gmail.com>
2018-04-14 21:43:35 -0600

    report service initial commit

---
+++ src/module/service/report.service.ts
@@ -0,0 +1,46 @@
+import {BaseService} from "./base-service";
+import {Http} from "@angular/http";
+import {Injectable} from "@angular/core";
+
+import {Subscription} from "rxjs/Subscription";
+import {Observable} from "rxjs/Observable";
+import moment from "moment";
+import 'rxjs/add/operator/toPromise';
+import "rxjs/add/operator/map";
+import 'rxjs/add/operator/catch';
+
+@Injectable()
+export class ReportService extends BaseService {
+
+    constructor(public http : Http) {
+        super();
+    }
+
+    public event(eventId: number): Promise<[any, any]> {
+        return this.wrapErrorHandler(
+            this.http.get(BaseService.Url + '/Reports/Event/' + eventId)
+            .toPromise()
+        )
+    }
+
+    public siteTotal(startDate: moment.Moment, endDate: moment.Moment): Promise<[any, any]> {
+        return this.wrapErrorHandler(
+            this.http.get(`${BaseService.Url}/Reports/Site/Total?filterStartDate=${startDate.format('L')}&filterEndDate=${endDate.format('L')}`)
+            .toPromise()
+        )
+    }
+
+    public siteTraffic(startDate: moment.Moment, endDate: moment.Moment): Promise<[any, any]> {
+        return this.wrapErrorHandler(
+            this.http.get(`${BaseService.Url}/Reports/Site/Traffic?filterStartDate=${startDate.format('L')}&filterEndDate=${endDate.format('L')}`)
+                .toPromise()
+        )
+    }
+
+    public volunteer(volunteerId: number, startDate: moment.Moment, endDate: moment.Moment): Promise<[any, any]> {
+        return this.wrapErrorHandler(
+            this.http.get(`${BaseService.Url}/Reports/Volunteer/${volunteerId}?filterStartDate=${startDate.format('L')}&filterEndDate=${endDate.format('L')}`)
+                .toPromise()
+        )   
+    }
+}
