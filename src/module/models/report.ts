39ef3d8826d207389bcccf605aee0060ae53ca11
amouritsen <amouritsen@gmail.com>
2018-04-17 14:12:06 -0600

    reports models using API info

--- src/module/models/report.ts
+++ src/module/models/report.ts
@@ -1,17 +1,38 @@
-import {BaseModel} from "./base-model";
+import { BaseModel } from './base-model';
+import { SiteEvent } from './site-event'
+import { Person } from './person'
+import { Volunteer } from './volunteer'
 
-export class SiteTotalsReport extends BaseModel {
-
+export class BaseReport extends BaseModel {
+    constructor(attributes: any) {
+        super(attributes);
+    }
 }
 
-export class SiteTrafficReport extends BaseModel {
-
+export class SiteTotalsReport extends BaseReport {
+    public items: Array<VolunteerStats>;
 }
 
-export class EventReport extends BaseModel {
-
+export class SiteTrafficReport extends BaseReport {
+    public people: Array<Person>;
 }
 
-export class VolunteerReport extends BaseModel {
+export class EventReport extends BaseReport {
+    public siteEvent: SiteEvent;
+    public people: Array<Person>;
+}
 
+export class VolunteerReport extends BaseReport {
+    public volunteer: Volunteer;
+    public totalHours: number;
+}
+
+export class VolunteerStats extends BaseModel {
+    public volunteer: Volunteer;
+    public totalHours: number;
+    public numberOfEvents: number;
+
+    constructor(attributes: any) {
+        super(attributes);
+    }
 }
