fe2d27c0f1534a3cc4f553e1d7204ad1f1a77e02
amouritsen <amouritsen@gmail.com>
2018-04-17 12:46:00 -0600

    report models initial commit

---
+++ src/module/models/report.ts
@@ -0,0 +1,17 @@
+import {BaseModel} from "./base-model";
+
+export class SiteTotalsReport extends BaseModel {
+
+}
+
+export class SiteTrafficReport extends BaseModel {
+
+}
+
+export class EventReport extends BaseModel {
+
+}
+
+export class VolunteerReport extends BaseModel {
+
+}
